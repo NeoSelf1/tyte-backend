c09d546c843094aa8799d70087d2bbd7f7610ee5
Hyeongseok Kim <102458207+NeoSelf1@users.noreply.github.com>
2024-12-12 07:10:19 +0900

    fix: 최신버전 롤백

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -15,7 +15,7 @@ let expirationString = '31d'
 authRouter.get('/version', async (req, res) => {
   try {
     console.log('versionCheck called from server')
-    return res.status(200).json({ newVersion: '1.2.1', minVersion: '1.2' })
+    return res.status(200).json({ newVersion: '1.2', minVersion: '1.2' })
   } catch (error) {
     console.error('Token validation error:', error)
     return res.status(500).json()
